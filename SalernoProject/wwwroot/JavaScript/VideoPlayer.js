be0c8da563eddd411757dfb9b53a030a94152861
zjmnk <zjsalerno94@gmail.com>
2024-01-08 01:13:20 -0600

    added videos and labels to reveal answers generate new opening after reavling answers.

---
+++ SalernoProject/wwwroot/JavaScript/VideoPlayer.js
@@ -0,0 +1,37 @@
+﻿var videoTimeout;
+function hideVideoAndPlay(video, videoSource, filePath, timeout)
+{
+    clearTimeout(videoTimeout);
+    var videoElement = document.getElementById(video);
+    var sourceElement = document.getElementById(videoSource);
+    sourceElement.src = filePath;
+    videoElement.style.display = "none";
+    videoElement.load();
+    videoElement.play();
+
+    videoTimeout = setTimeout(function () {
+        videoElement.pause();
+        videoElement.currentTime = 0;
+    }, timeout);
+
+}
+
+function showVideoAndPlay(video, videoSource, filePath, dotNetHelper)
+{
+    clearTimeout(videoTimeout);
+    var videoElement = document.getElementById(video);
+    var sourceElement = document.getElementById(videoSource);
+    sourceElement.src = filePath;
+    videoElement.style.display = "block";
+    videoElement.load();
+    videoElement.play();
+
+    videoTimeout = setTimeout(function () {
+        videoElement.pause();
+        videoElement.currentTime = 0;
+        videoElement.style.display = "none";
+        dotNetHelper.invokeMethodAsync("NewOpening");
+        
+    }, 10000);
+
+}
