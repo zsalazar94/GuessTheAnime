d433f4b2cf670cb1e93bdcf7def1c4e12acf4109
zjmnk <zjsalerno94@gmail.com>
2024-01-07 23:49:02 -0600

    Added funtionality to play audio, choose length, and choose new songs.

---
+++ SalernoProject/Data/AudioFile.cs
@@ -0,0 +1,18 @@
+﻿namespace SalernoProject.Data
+{
+    public class AudioFile
+    {
+        public string filePath { get; set; } 
+        public string animeName { get; set; }
+        public string openingNumber { get; set;}
+        public string openingName { get; set; }
+
+        public AudioFile(string animeName, string openingNumber, string openingName, string filePath)
+        {
+            this.animeName = animeName;
+            this.openingNumber = openingNumber;
+            this.openingName = openingName;
+            this.filePath = filePath;
+        }
+    }
+}
---
+++ SalernoProject/wwwroot/JavaScript/AudioPlayer.js
@@ -0,0 +1,13 @@
+﻿function setAudioSourceAndPlay(audio, audioSource, filePath, timeout)
+{
+    var audioElement = document.getElementById(audio);
+    var sourceElement = document.getElementById(audioSource);
+    sourceElement.src = filePath;
+    audioElement.load();
+    audioElement.play();
+
+    setTimeout(function () {
+        audioElement.pause();
+        audioElement.currentTime = 0;
+    }, timeout);
+}
