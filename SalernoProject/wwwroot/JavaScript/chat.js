6a63b05bbefc75e8e5642938e54538a39cc6e764
zjmnk <zjsalerno94@gmail.com>
2024-02-04 17:23:18 -0600

    Added keep view at bottom of chat when new messages come in

--- SalernoProject/wwwroot/JavaScript/chat.js
+++ SalernoProject/wwwroot/JavaScript/chat.js
@@ -1,4 +1,15 @@
-﻿function blurElementById(elementId) {
+﻿function scrollToBottom() {
+    var messagesList = document.getElementById('messagesList');
+    messagesList.scrollTop = messagesList.scrollHeight;
+}
+
+function checkIfAtBottomOfChat() {
+    var messagesList = document.getElementById('messagesList');
+    var shouldScroll = messagesList.scrollHeight - messagesList.scrollTop === messagesList.clientHeight;
+    return shouldScroll;
+}
+
+function blurElementById(elementId) {
     const element = document.getElementById(elementId);
     if (element) {
         element.blur();
