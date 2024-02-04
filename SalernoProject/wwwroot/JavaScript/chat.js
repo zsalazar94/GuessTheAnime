4ac32a6d1b0639289bf940782ec9e6f4ff4f42e5
zjmnk <zjsalerno94@gmail.com>
2024-02-04 14:13:55 -0600

    fixed double sending messages and improved css

---
+++ SalernoProject/wwwroot/JavaScript/chat.js
@@ -0,0 +1,13 @@
+﻿function blurElementById(elementId) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.blur();
+    }
+}
+
+function focusElementById(elementId) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.focus();
+    }
+}
